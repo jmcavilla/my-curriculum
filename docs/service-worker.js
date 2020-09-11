bc053faca4b8c708c133eedf55843a8b01c1fea8
Juan Manuel Cavilla <jcavilla@quadiontech.com>
2020-09-10 21:00:47 -0300

    Prueba para corregir PUBLIC_URL

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.663a523e155222528ff9dae65514984d.js"
+  "./precache-manifest.3922ccad8554cedc15a2855d1f67a9ab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
