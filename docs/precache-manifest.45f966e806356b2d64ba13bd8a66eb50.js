f8594fda3f0bfd04af38829fb5e24f88130d9a4a
jcavilla <jcavilla@quadiontech.com>
2021-04-20 14:43:08 -0300

    Actualizacion web

---
+++ docs/precache-manifest.45f966e806356b2d64ba13bd8a66eb50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d24e13254fd376ffdb6ce2505201934",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc798cd63e7d396c7ffb",
+    "url": "./static/css/main.df1a49dc.chunk.css"
+  },
+  {
+    "revision": "b5d4e367560a244a289d",
+    "url": "./static/js/2.2b0ceccf.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "./static/js/2.2b0ceccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc798cd63e7d396c7ffb",
+    "url": "./static/js/main.12ea655d.chunk.js"
+  },
+  {
+    "revision": "320f2da506fb81cb6efb",
+    "url": "./static/js/runtime-main.f85f8683.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fd33dcd93659597cbb72b608aac6abfd.js"
+  "./precache-manifest.45f966e806356b2d64ba13bd8a66eb50.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
